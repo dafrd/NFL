176ddee4d5b6d8cf7325d2813e10ca69546d25ba
dafrd <fredericprigent@gmail.com>
2022-08-05 17:19:31 +0200

    commit

--- nflscraper/nflscraper/spiders/espnscores.py
+++ nflscraper/nflscraper/spiders/espnscores.py
@@ -11,13 +11,12 @@ class ESPNScoresSpider(scrapy.Spider):
             #try:
                 yield{
                     'awayteam': scores.css('div.ScoreCell__TeamName.ScoreCell__TeamName--shortDisplayName.truncate.db::text').get(),
-                    'hometeam': scores.css('div.ScoreCell__TeamName.ScoreCell__TeamName--shortDisplayName.truncate.db::text').extract()[1]
-,
+                    'hometeam': scores.css('div.ScoreCell__TeamName.ScoreCell__TeamName--shortDisplayName.truncate.db::text').extract()[1],
                 }
             #except:
         #return super().parse(response, **kwargs)
-        next_page = response.css('a.nfl-o-table-pagination__next').attrib['href']
-        if next_page is not None:
-            yield response.follow(next_page, callback=self.parse)
+        #next_page = response.css('a.nfl-o-table-pagination__next').attrib['href']
+        #if next_page is not None:
+        #    yield response.follow(next_page, callback=self.parse)
 
 
