834cc1aff2e52d36a5a9601995e935514047217b
dafrd <fredericprigent@gmail.com>
2022-08-06 11:50:56 +0200

    commit

--- nflscraping/nflscraping/settings.py
+++ nflscraping/nflscraping/settings.py
@@ -17,7 +17,7 @@ NEWSPIDER_MODULE = 'nflscraping.spiders'
 #USER_AGENT = 'nflscraping (+http://www.yourdomain.com)'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
