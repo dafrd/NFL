d24aa7d72f5eb0cf0e6175464de00a219efbf057
dafrd <fredericprigent@gmail.com>
2022-08-09 09:24:16 +0200

    push

--- nflscraping/nflscraping/spiders/espn_games_detail.py
+++ nflscraping/nflscraping/spiders/espn_games_detail.py
@@ -40,102 +40,103 @@ class ESPNGamesDetailSpider(scrapy.Spider):
                         home.append(player.css('td.name > a > span::text').get())
                     home = list(set(home))
         
-        
-            yield{
-                "game_id" : split[7],
-                "away roster" : away,
-                "home roster" : home,
-                }
+        yield{
+            "game_id" : split[7],
+            "away roster" : away,
+            "home roster" : home,
+            }
 
-            for name in away:
-                for player in response.css('tr:contains("'+name+'")'):
-                    player_abbr = player.css('span.abbr::text').get()
-                    player_name = player.css('td.name > a > span::text').get()
-                    if player_abbr is not None:
-                        yield{
+        for player in away:
+            #for player in response.css('tr:contains("'+name+'")'):
+            #player_name = player.xpath('//span[text()='+player+']/div/div[1]/div/div/table/tbody/tr[1]/td[1]/a/span[1]).get())
+            #player_name = player.css('td.name > a > span::text').get()
+            #if player_abbr is not None:
+            yield{
+                    "game_id" : split[7],
+                    "team" : response.css('span.team-name::text').get(),
+                    "player_name" : response.xpath('//span[text()="'+player+'"]/text()').get(),
+                    "player_url" : response.xpath('//span[text()="'+player+'"]/ancestor::a/@href').get(),
+                    "pass completion" : response.xpath('//*[@id="gamepackage-passing"]/div/div[1]/div/div/table/tbody/tr/td/a/span[text()="'+player+'"]/../../../td[@class="c-att"]/text()').get(),
+                    "pass yds" : response.xpath('//*[@id="gamepackage-passing"]/div/div[1]/div/div/table/tbody/tr/td/a/span[text()="'+player+'"]/../../../td[@class="yds"]/text()').get(),
+                    "pass avg" : response.xpath('//*[@id="gamepackage-passing"]/div/div[1]/div/div/table/tbody/tr/td/a/span[text()="'+player+'"]/../../../td[@class="avg"]/text()').get(),
+                    "pass td" : response.xpath('//*[@id="gamepackage-passing"]/div/div[1]/div/div/table/tbody/tr/td/a/span[text()="'+player+'"]/../../../td[@class="td"]/text()').get(),
+                    "pass int" : response.xpath('//*[@id="gamepackage-passing"]/div/div[1]/div/div/table/tbody/tr/td/a/span[text()="'+player+'"]/../../../td[@class="int"]/text()').get(),
+                    "sacks" : response.xpath('//*[@id="gamepackage-passing"]/div/div[1]/div/div/table/tbody/tr/td/a/span[text()="'+player+'"]/../../../td[@class="sacks"]/text()').get(),
+                    "qbr" : response.xpath('//*[@id="gamepackage-passing"]/div/div[1]/div/div/table/tbody/tr/td/a/span[text()="'+player+'"]/../../../td[@class="qbr"]/text()').get(),
+                    "rtg" : response.xpath('//*[@id="gamepackage-passing"]/div/div[1]/div/div/table/tbody/tr/td/a/span[text()="'+player+'"]/../../../td[@class="rtg"]/text()').get(),
+                    "rush car" : response.xpath('//*[@id="gamepackage-rushing"]/div/div[1]/div/div/table/tbody/tr/td/a/span[text()="'+player+'"]/../../../td[@class="car"]/text()').get(),
+                    "rush yds" : response.xpath('//*[@id="gamepackage-rushing"]/div/div[1]/div/div/table/tbody/tr/td/a/span[text()="'+player+'"]/../../../td[@class="yds"]/text()').get(),
+                    "rush avg" : response.xpath('//*[@id="gamepackage-rushing"]/div/div[1]/div/div/table/tbody/tr/td/a/span[text()="'+player+'"]/../../../td[@class="avg"]/text()').get(),
+                    "rush td" : response.xpath('//*[@id="gamepackage-rushing"]/div/div[1]/div/div/table/tbody/tr/td/a/span[text()="'+player+'"]/../../../td[@class="td"]/text()').get(),
+                    "rush long" : response.xpath('//*[@id="gamepackage-rushing"]/div/div[1]/div/div/table/tbody/tr/td/a/span[text()="'+player+'"]/../../../td[@class="long"]/text()').get(),
+                    "receptions" : response.xpath('//*[@id="gamepackage-receiving"]/div/div[1]/div/div/table/tbody/tr/td/a/span[text()="'+player+'"]/../../../td[@class="rec"]/text()').get(),
+                    "rec yds" : response.xpath('//*[@id="gamepackage-receiving"]/div/div[1]/div/div/table/tbody/tr/td/a/span[text()="'+player+'"]/../../../td[@class="yds"]/text()').get(),
+                    "rec avg" : response.xpath('//*[@id="gamepackage-receiving"]/div/div[1]/div/div/table/tbody/tr/td/a/span[text()="'+player+'"]/../../../td[@class="avg"]/text()').get(),  
+                    "rec td" : response.xpath('//*[@id="gamepackage-receiving"]/div/div[1]/div/div/table/tbody/tr/td/a/span[text()="'+player+'"]/../../../td[@class="td"]/text()').get(),  
+                    "rec long" : response.xpath('//*[@id="gamepackage-receiving"]/div/div[1]/div/div/table/tbody/tr/td/a/span[text()="'+player+'"]/../../../td[@class="long"]/text()').get(),
+                    "rec tgs" : response.xpath('//*[@id="gamepackage-receiving"]/div/div[1]/div/div/table/tbody/tr/td/a/span[text()="'+player+'"]/../../../td[@class="tgts"]/text()').get(),
+                    "fumbles" : response.xpath('//*[@id="gamepackage-fumbles"]/div/div[1]/div/div/table/tbody/tr/td/a/span[text()="'+player+'"]/../../../td[@class="fum"]/text()').get(),
+                    "fumbles lost" : response.xpath('//*[@id="gamepackage-fumbles"]/div/div[1]/div/div/table/tbody/tr/td/a/span[text()="'+player+'"]/../../../td[@class="lost"]/text()').get(),
+                    "fumbles rec" : response.xpath('//*[@id="gamepackage-fumbles"]/div/div[1]/div/div/table/tbody/tr/td/a/span[text()="'+player+'"]/../../../td[@class="rec"]/text()').get()          
+        }
+
+        for player in home:
+            #for player in response.css('tr:contains("'+name+'")'):
+            #player_name = player.xpath('//span[text()='+player+']/div/div[1]/div/div/table/tbody/tr[1]/td[1]/a/span[1]).get())
+            #player_name = player.css('td.name > a > span::text').get()
+            #if player_abbr is not None:
+            yield{
+                    "game_id" : split[7],
+                    "team" : response.css('span.team-name::text').get(),
+                    "player_name" : response.xpath('//span[text()="'+player+'"]/text()').get(),
+                    "player_url" : response.xpath('//span[text()="'+player+'"]/ancestor::a/@href').get(),
+                    "pass completion" : response.xpath('//*[@id="gamepackage-passing"]/div/div[1]/div/div/table/tbody/tr/td/a/span[text()="'+player+'"]/../../../td[@class="c-att"]/text()').get(),
+                    "pass yds" : response.xpath('//*[@id="gamepackage-passing"]/div/div[1]/div/div/table/tbody/tr/td/a/span[text()="'+player+'"]/../../../td[@class="yds"]/text()').get(),
+                    "pass avg" : response.xpath('//*[@id="gamepackage-passing"]/div/div[1]/div/div/table/tbody/tr/td/a/span[text()="'+player+'"]/../../../td[@class="avg"]/text()').get(),
+                    "pass td" : response.xpath('//*[@id="gamepackage-passing"]/div/div[1]/div/div/table/tbody/tr/td/a/span[text()="'+player+'"]/../../../td[@class="td"]/text()').get(),
+                    "pass int" : response.xpath('//*[@id="gamepackage-passing"]/div/div[1]/div/div/table/tbody/tr/td/a/span[text()="'+player+'"]/../../../td[@class="int"]/text()').get(),
+                    "sacks" : response.xpath('//*[@id="gamepackage-passing"]/div/div[1]/div/div/table/tbody/tr/td/a/span[text()="'+player+'"]/../../../td[@class="sacks"]/text()').get(),
+                    "qbr" : response.xpath('//*[@id="gamepackage-passing"]/div/div[1]/div/div/table/tbody/tr/td/a/span[text()="'+player+'"]/../../../td[@class="qbr"]/text()').get(),
+                    "rtg" : response.xpath('//*[@id="gamepackage-passing"]/div/div[1]/div/div/table/tbody/tr/td/a/span[text()="'+player+'"]/../../../td[@class="rtg"]/text()').get(),
+                    "rush car" : response.xpath('//*[@id="gamepackage-rushing"]/div/div[1]/div/div/table/tbody/tr/td/a/span[text()="'+player+'"]/../../../td[@class="car"]/text()').get(),
+                    "rush yds" : response.xpath('//*[@id="gamepackage-rushing"]/div/div[1]/div/div/table/tbody/tr/td/a/span[text()="'+player+'"]/../../../td[@class="yds"]/text()').get(),
+                    "rush avg" : response.xpath('//*[@id="gamepackage-rushing"]/div/div[1]/div/div/table/tbody/tr/td/a/span[text()="'+player+'"]/../../../td[@class="avg"]/text()').get(),
+                    "rush td" : response.xpath('//*[@id="gamepackage-rushing"]/div/div[1]/div/div/table/tbody/tr/td/a/span[text()="'+player+'"]/../../../td[@class="td"]/text()').get(),
+                    "rush long" : response.xpath('//*[@id="gamepackage-rushing"]/div/div[1]/div/div/table/tbody/tr/td/a/span[text()="'+player+'"]/../../../td[@class="long"]/text()').get(),
+                    "receptions" : response.xpath('//*[@id="gamepackage-receiving"]/div/div[1]/div/div/table/tbody/tr/td/a/span[text()="'+player+'"]/../../../td[@class="rec"]/text()').get(),
+                    "rec yds" : response.xpath('//*[@id="gamepackage-receiving"]/div/div[1]/div/div/table/tbody/tr/td/a/span[text()="'+player+'"]/../../../td[@class="yds"]/text()').get(),
+                    "rec avg" : response.xpath('//*[@id="gamepackage-receiving"]/div/div[1]/div/div/table/tbody/tr/td/a/span[text()="'+player+'"]/../../../td[@class="avg"]/text()').get(),  
+                    "rec td" : response.xpath('//*[@id="gamepackage-receiving"]/div/div[1]/div/div/table/tbody/tr/td/a/span[text()="'+player+'"]/../../../td[@class="td"]/text()').get(),  
+                    "rec long" : response.xpath('//*[@id="gamepackage-receiving"]/div/div[1]/div/div/table/tbody/tr/td/a/span[text()="'+player+'"]/../../../td[@class="long"]/text()').get(),
+                    "rec tgs" : response.xpath('//*[@id="gamepackage-receiving"]/div/div[1]/div/div/table/tbody/tr/td/a/span[text()="'+player+'"]/../../../td[@class="tgts"]/text()').get(),
+                    "fumbles" : response.xpath('//*[@id="gamepackage-fumbles"]/div/div[1]/div/div/table/tbody/tr/td/a/span[text()="'+player+'"]/../../../td[@class="fum"]/text()').get(),
+                    "fumbles lost" : response.xpath('//*[@id="gamepackage-fumbles"]/div/div[1]/div/div/table/tbody/tr/td/a/span[text()="'+player+'"]/../../../td[@class="lost"]/text()').get(),
+                    "fumbles rec" : response.xpath('//*[@id="gamepackage-fumbles"]/div/div[1]/div/div/table/tbody/tr/td/a/span[text()="'+player+'"]/../../../td[@class="rec"]/text()').get()          
+        }
+
+            '''
+            for game in response.css('#gamepackage-box-score'):
+                for awayteam in game.css('div.col.column-one.gamepackage-away-wrap'):
+                    for player in awayteam.css('td.name'):
+                        player_name = player.css('span.abbr::text').get()
+                        if player_name is not None:
+                            yield{
                                 "game_id" : split[7],
                                 "team" : response.css('span.team-name::text').get(),
                                 "player_name" : player.css('td.name > a > span::text').get(),
                                 "player_url" : player.css('td.name > a::attr(href)').get(),
-                                "pass completion" : player.css('td.c-att::text').get(),
-                                #"pass yds" : player.xpath('//*[@id="gamepackage-passing"]/div/div[1]/div/div/table/tbody/tr/td[3]/text()').get(),
-                                "pass td" : player.css('td.td::text').get(),
-                                "pass int" : player.css('td.int::text').get(),
-                                "sacks" : player.css('td.sacks::text').get(),
-                                "qbr" : player.css('td.qbr::text').get(),
-                                "rtg" : player.css('td.rtg::text').get(),
-                                "rush car" : player.css('td.car::text').get(),
-                                #"rush td" : awayteam.css('td.td::text').get(),
-                                #"rush yds" : awayteam.css('td.yds::text').get(),
-                                #"rush long" : player.css('td.long::text').get(),
-                                #"rush avg" : player.css('td.avg::text').get(),
-                                "receptions" : player.css('td.rec::text').get(),
-                                #"rec yds" : player.css('td.yds::text').get(),
-                                #"rec avg" : player.css('td.avg::text').get(),  
-                                #"rec td" : awayteam.css('td.yds::text').get(),  
-                                #"rec long" : player.css('td.long::text').get(),
-                                "rec tgs" : player.css('td.tgts::text').get(),
-                                "fumble" : player.css('td.fum::text').get(),
-                                "lost" : player.css('td.lost::text').get(),
-                                #"fumbles rec" : pl          
-                    }
-        
-        for name in home:
-            for player in response.css('tr:contains("'+name+'")'):
-                player_abbr = player.css('span.abbr::text').get()
-                player_name = player.css('td.name > a > span::text').get()
-                if player_abbr is not None:
-                    yield{
-                                "game_id" : split[7],
-                                "team" : response.css('span.team-name::text').extract()[1],
-                                "player_name" : player.css('td.name > a > span::text').get(),
-                                "player_url" : player.css('td.name > a::attr(href)').get(),
-                                "pass completion" : player.css('td.c-att::text').get(),
-                                #"pass yds" : player.xpath('//*[@id="gamepackage-passing"]/div/div[1]/div/div/table/tbody/tr/td[3]/text()').get(),
-                                "pass td" : player.css('td.td::text').get(),
-                                "pass int" : player.css('td.int::text').get(),
-                                "sacks" : player.css('td.sacks::text').get(),
-                                "qbr" : player.css('td.qbr::text').get(),
-                                "rtg" : player.css('td.rtg::text').get(),
-                                "rush car" : player.css('td.car::text').get(),
-                                #"rush td" : awayteam.css('td.td::text').get(),
-                                #"rush yds" : awayteam.css('td.yds::text').get(),
-                                #"rush long" : player.css('td.long::text').get(),
-                                #"rush avg" : player.css('td.avg::text').get(),
-                                "receptions" : player.css('td.rec::text').get(),
-                                #"rec yds" : player.css('td.yds::text').get(),
-                                #"rec avg" : player.css('td.avg::text').get(),  
-                                #"rec td" : awayteam.css('td.yds::text').get(),  
-                                #"rec long" : player.css('td.long::text').get(),
-                                "rec tgs" : player.css('td.tgts::text').get(),
-                                "fumble" : player.css('td.fum::text').get(),
-                                "lost" : player.css('td.lost::text').get(),
-                                #"fumbles rec" : pl          
-                    }
-
-        '''
-        for game in response.css('#gamepackage-box-score'):
-            for awayteam in game.css('div.col.column-one.gamepackage-away-wrap'):
-                for player in awayteam.css('td.name'):
-                    player_name = player.css('span.abbr::text').get()
-                    if player_name is not None:
-                        yield{
-                            "game_id" : split[7],
-                            "team" : response.css('span.team-name::text').get(),
-                            "player_name" : player.css('td.name > a > span::text').get(),
-                            "player_url" : player.css('td.name > a::attr(href)').get(),
-                            "pass completion" : awayteam.css('td.c-att::text').get(),
-                            "pass yds" : awayteam.css('td.yds::text').get(),
-                            "pass avg" : awayteam.css('td.avg::text').get(),
-                            "pass td" : awayteam.css('td.td::text').get(),
-                            "pass int" : awayteam.css('td.int::text').get(),
-                            "sacks" : awayteam.css('td.sacks::text').get(),
-                            "qbr" : awayteam.css('td.qbr::text').get(),
-                            "rtg" : awayteam.css('td.rtg::text').get(),
-                            "rush car" : awayteam.css('td.car::text').get(),
-                            "rush yds" : awayteam.css('td.yds::text').get(),
-                            "rush long" : awayteam.css('td.long::text').get()
-                        }
+                                "pass completion" : awayteam.css('td.c-att::text').get(),
+                                "pass yds" : awayteam.css('td.yds::text').get(),
+                                "pass avg" : awayteam.css('td.avg::text').get(),
+                                "pass td" : awayteam.css('td.td::text').get(),
+                                "pass int" : awayteam.css('td.int::text').get(),
+                                "sacks" : awayteam.css('td.sacks::text').get(),
+                                "qbr" : awayteam.css('td.qbr::text').get(),
+                                "rtg" : awayteam.css('td.rtg::text').get(),
+                                "rush car" : awayteam.css('td.car::text').get(),
+                                "rush yds" : awayteam.css('td.yds::text').get(),
+                                "rush long" : awayteam.css('td.long::text').get()
+                            }
                 
             for hometeam in game.css('div.col.column-two.gamepackage-away-wrap'):
                 for player in hometeam.css('td.name'):
